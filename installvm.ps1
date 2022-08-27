08b7bc33b321ca9dc99508214dde38cf892266c0
= <=>
2022-08-27 10:56:18 +0530

    New PS1 created for RG Creation

---
+++ installvm.ps1
@@ -0,0 +1,28 @@
+param(
+    [# Parameter help description]
+    [Parameter(Mandatory=$true)][string] $RGLocation,
+    [Parameter(Mandatory=$true)][string] $RGName
+)
+$ErrorActionPreference  = "stop"
+[console]::ResetColor()
+$ScriptName = $MyInvocation.MyCommand.Name
+
+Write-host "----------------------------------------------------"
+Write-Host resourcegroup location is $RGLocation
+Write-Host resourcegroup name is $RGName
+Write-host "----------------------------------------------------"
+#Check RG Status
+$RGStatus = az group exists -n $RGName
+az group create -l $RGLocation -n $RGName
+
+if ($RGStatus -eq FALSE)
+{
+    az group create -l $RGLocation -n $RGName
+}
+else{
+    Write-Host resourcegroup $RGName already exists
+}
+#======================
+Write-Host "End of Script ($ScriptName)"
+
+
